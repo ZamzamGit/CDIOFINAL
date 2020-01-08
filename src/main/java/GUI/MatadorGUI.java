ea59907a7d18ed18a460a676840e85af874fef39
Khaled <s195487@student.dtu.dk>
2020-01-08 12:45:02 +0100

    guifelter

--- src/main/java/GUI/MatadorGUI.java
+++ src/main/java/GUI/MatadorGUI.java
@@ -1,8 +1,6 @@
 package GUI;
 
-import gui_fields.GUI_Field;
-import gui_fields.GUI_Start;
-import gui_fields.GUI_Street;
+import gui_fields.*;
 import gui_main.GUI;
 
 import java.awt.*;
@@ -14,13 +12,27 @@ public class MatadorGUI {
 
     public static void GUIfelter() {
 
-        GUI_Field[] field = new GUI_Field[2];
+        GUI_Field[] field = new GUI_Field[4];
 
         GUI_Start start = new GUI_Start("Start", "200$", "", Color.WHITE, Color.BLACK);
         field[0] = start;
 
-        GUI_Street choko = new GUI_Street("Chokolade", "200$", "", "200", Color.RED, Color.BLACK);
+        GUI_Street choko = new GUI_Street("Chokolade", "200$", "", "200", Color.ORANGE, Color.BLACK);
         field[1] = choko;
+
+        GUI_Chance chance = new GUI_Chance("?", "Chance", "", Color.WHITE, Color.BLACK);
+        field[2] = chance;
+
+        GUI_Tax tax = new GUI_Tax();
+        field[3] = tax;
+
+
+
+
+
+
+        GUI gui = new GUI(field);
+
 
 
         //GUI_Start start = new GUI_Start("START", "200$", "200", Color.WHITE, Color.BLACK);
